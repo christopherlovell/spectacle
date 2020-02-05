0c181406d2fb33454c53d41bf7a4ff3ef9e2fa36
christopherlovell <chrisl1991@hotmail.co.uk>
2020-02-05 17:28:02 +0000

    save scale factor in bpass output

--- grids/grid_bpass.py
+++ grids/grid_bpass.py
@@ -2,6 +2,9 @@ from hoki import load
 import numpy as np
 from glob import glob
 from spectacle.h5py_utils import write_data_h5py
+from astropy.cosmology import Planck15 as cosmo
+from astropy.cosmology import z_at_value
+import astropy.units as u
 
 model_dir = 'BPASSv2.2.1_sin-imf_chab300'
 fname = 'output/bpass_sin.h5'
@@ -11,9 +14,14 @@ models = glob(model_dir+'/*')
 
 output_temp = load.model_output(models[0])
 
+print(cosmo.age(0).value)
 ages = np.array([float(a) for a in output_temp.columns[1:]])
-age_mask = (10**ages / 1e9) < 18 # Gyr
+ages_Gyr = 10**ages / 1e9 # Gyr
+age_mask = ages_Gyr < cosmo.age(0).value - 0.4 # Gyr
 ages = ages[age_mask]
+ages_Gyr = ages_Gyr[age_mask]
+
+scale_factors = cosmo.scale_factor([z_at_value(cosmo.lookback_time, age * u.Gyr) for age in ages_Gyr])
 
 wl = output_temp['WL'].values
 metallicities = np.array([None] * len(models))
@@ -43,13 +51,12 @@ for i,(Z,mod) in enumerate(zip(metallicities,np.array(models)[Z_idx])):
 
 
 # convert units
-ages = 10**ages / 1e9 # Gyr
 metallicities = np.log10(metallicities / 0.0127)  # log(Z / Zsol)
 spec /= 1e6 # Msol
 
 
 write_data_h5py(fname,'spec',data=spec,overwrite=True)
-write_data_h5py(fname,'ages',data=ages,overwrite=True)
+write_data_h5py(fname,'ages',data=scale_factors,overwrite=True)
 write_data_h5py(fname,'metallicities',data=metallicities,overwrite=True)
 write_data_h5py(fname,'wavelength',data=wl,overwrite=True)
 
